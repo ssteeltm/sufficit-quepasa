f194881a2e68bda744b9c728faba5da8f7618723
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2022-10-25 10:59:48 -0300

    comments

--- src/controllers/api_bot_extensions.go
+++ src/controllers/api_bot_extensions.go
@@ -8,12 +8,24 @@ import (
 	models "github.com/sufficit/sufficit-quepasa/models"
 )
 
-//<summary>Find a whatsapp server by token passed on Url Path parameters</summary>
+/*
+<summary>
+	Find a whatsapp server by token passed on Url Path parameters
+</summary>
+*/
 func GetServer(r *http.Request) (server *models.QPWhatsappServer, err error) {
 	token := GetToken(r)
 	return models.GetServerFromToken(token)
 }
 
+/*
+<summary>
+	Get Token From Http Request
+	1º Url Param (/:token/)
+	2º Url Query (?token=)
+	3º Header (X-QUEPASA-TOKEN)
+</summary>
+*/
 func GetToken(r *http.Request) (result string) {
 
 	// retrieve from url path parameter
